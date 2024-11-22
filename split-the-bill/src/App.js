46eaf409ad7ea36a808b827e787d1466535fd2f4
Jaswant Maurya <138619428+jasss24@users.noreply.github.com>
2024-11-22 19:41:18 +0530

    App.js

--- split-the-bill/src/App.js
+++ split-the-bill/src/App.js
@@ -113,12 +113,12 @@ function Friend({ friend, onSelection, selectedFriend }) {
 
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.name} {Math.abs(friend.balance)}€
+          You owe {friend.name} {Math.abs(friend.balance)} Rs.
         </p>
       )}
       {friend.balance > 0 && (
         <p className="green">
-          {friend.name} owes you {Math.abs(friend.balance)}€
+          {friend.name} owes you {Math.abs(friend.balance)} Rs.
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
